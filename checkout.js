2445d1f3df907ed5acafb687965e1388999c945e
RyuKam1 <omarashvili.giorgi07@gmail.com>
2024-03-09 04:55:32 +0400

    dd

--- checkout.js
+++ checkout.js
@@ -1,13 +1,11 @@
-import { loadStripe } from "@stripe/stripe-js";
 
 export async function checkout({lineItems}){
 const stripe = require('stripe')(`${process.env.NEXT_PUBLIC_API_KEY}`);
 
 const session = await stripe.checkout.sessions.create({
-    
-    line_items,
+    line_items: lineItems,
     mode: 'payment',
-    successUrl:`${window.location.origin}?session_id{CHECKOUT_SESSION_ID}`,
-    cancelUrl: `${window.location.origin}`,
+    success_url:`${window.location.origin}?session_id{CHECKOUT_SESSION_ID}`,
+    cancel_url: `${window.location.origin}`,
 });
 }
