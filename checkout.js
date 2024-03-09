93a445803257978c01d0fce060e845caca2ab732
RyuKam1 <omarashvili.giorgi07@gmail.com>
2024-03-09 04:04:01 +0400

    fixing donation

--- checkout.js
+++ checkout.js
@@ -5,14 +5,14 @@ export async function checkout({lineItems}){
 
     const getStripe = () => {
         if(!stripePromise){
-            stripePromise = loadStripe(process.env.NEXT_PUBLIC_API_KEY)
+            stripePromise = loadStripe(`${process.env.NEXT_PUBLIC_API_KEY}`)
         }
         return stripePromise
     }
 
     const stripe = await getStripe()
     await stripe.redirectToCheckout({
-        mode: 'donation',
+        mode: 'payment',
         lineItems,
         successUrl:`${window.location.origin}?session_id{CHECKOUT_SESSION_ID}`,
         cancelUrl: `${window.location.origin}`,
