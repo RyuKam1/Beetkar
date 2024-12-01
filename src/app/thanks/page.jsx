afc32df312da74d4175409a296c024da0da3fdb7
RyuKam1 <omarashvili.giorgi07@gmail.com>
2024-12-01 20:28:17 +0400

    Improved Email Sending API

--- src/app/thanks/page.jsx
+++ src/app/thanks/page.jsx
@@ -30,6 +30,46 @@ const page = async ({ params, searchParams }) => {
     console.error("Error sending email:", error);
   }
 
+  try {
+    const response = await resend.emails.send({
+      from: "Beetkar <beetkar@beetkar.online>",
+      to: "omarashvili.giorgi07@gmail.com",
+      subject: "New Order!",
+      name: "Beetkar",
+      html: `<h1>New Order!</h1><p>Name: ${searchParams.name} <br/> Email: ${searchParams.email} <br/> Phone: ${searchParams.phone} <br/> Amount: ${searchParams.amount} <br/></p>`,
+    });
+
+    console.log("Email sent successfully:", response);
+    console.log(
+      searchParams.name,
+      searchParams.email,
+      searchParams.phone,
+      searchParams.amount
+    );
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+
+  try {
+    const response = await resend.emails.send({
+      from: "Beetkar <beetkar@beetkar.online>",
+      to: `${searchParams.email}`,
+      subject: "New Order!",
+      name: "Message From Beetkar",
+      html: `<h1>Thank You ${searchParams.name} For Your Request!</h1><p>Hello ${searchParams.name}. We have received your request and we will get in contact with you as soon as possible. <br/> <br/>We appriciate that you are interested in our business idea.<br/> <br/>Sincerely <br/> <br/>Beetkar Team.</p>`,
+    });
+
+    console.log("Email sent successfully:", response);
+    console.log(
+      searchParams.name,
+      searchParams.email,
+      searchParams.phone,
+      searchParams.amount
+    );
+  } catch (error) {
+    console.error("Error sending email:", error);
+  }
+
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
